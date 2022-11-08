52406107e43819f20f2ec09f0c9eac501b436c34
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-09 03:35:14 +0530

    Working on Item Details

--- src/Components/ItemDetails.js
+++ src/Components/ItemDetails.js
@@ -7,13 +7,16 @@ function ItemDetails() {
  const item = location.state;
 
   return (
-    <>
-        <div className='itemdetails-img'>
-            
-            <img src={item.imageUrl} alt="" />
+        <div className='imagedetails-main'>
+                <div className='imagedetails-img'>
+                    <img src={item.imageUrl} alt="" />
+                </div>  
 
+                <div className="itemdetails-alldetails">
+                    <h1 style={{fontWeight : '650', fontSize : '2.2rem'}}>{item.title}</h1>
+                    <h2 style={{color:'blue'}}>M.R.P. <span style={{color : 'yellow'}}>Hello</span></h2>
+                </div>
         </div>
-    </>
   )
 }
 
