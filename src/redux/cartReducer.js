e10c67213a0af9c46b0ebc0e5564e2b48e3cec3e
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-14 01:46:05 +0530

    Immutable Changes for increasing cart items

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -7,25 +7,44 @@ const CartReducer = (state = initalState, action) => {
     switch (action.type) {
         case 'ADD_ITEMS':
             let item = action.payload;
+            item.count = 1;
             let arr = [...state.value];
             
-            // arr = [ {id : 1, Name : 'a} ]
-            // item = {id : 1, Name : 'a'}
-
-            let isItemExist = arr.find((e)=> e.id === item.id)
-
-            // isItemExist = {id : 1, Name : 'a'}
-
-            console.log(isItemExist);
-
-            // arr.forEach((e)=> {   // shi appraoch hai
-            //     if(e.id === item.id) isItemExist = true  
-            // })
-
-            if(!isItemExist){
+            if(arr.length === 0){
                 state = {
                     ...state,
-                    value : [...state.value, item]  
+                    value : [...state.value, item]
+                }
+            }
+            else{
+                let bool = false;
+                
+                arr.forEach((e)=> {
+                    if(e.id === item.id) bool = true
+                })
+                if(!bool){
+                    state = {
+                        ...state,
+                        value : [...state.value, item]  
+                    }
+                }
+                else{
+                    
+                    let newArr = [...state.value];
+                    newArr.forEach((e, idx)=> {
+                        if(e.id === item.id){
+                            let res = [...state.value][idx]
+                            res.count += 1;
+
+                            const ans = JSON.parse(JSON.stringify(newArr));
+
+                            state = {
+                                ...state,
+                                value : ans
+                            }
+
+                        }
+                    })
                 }
             }     
 
--- test.js
+++ test.js
@@ -1,24 +1,23 @@
-let state = {
-    value : [{id : 1}, {id : 2}, {id : 3}, {id : 4}]
-}
-let item = {id : 4};
+// let state = {
+//     value : [{id : 1, count : 1}, {id : 2, count : 1}, {id : 3, count : 1}, {id : 4, count : 1}]
+// }
 
-let bool = false;
+// let item = {
+//     id : 10
+// }
+// item.count = 1;
 
-
-// [...state.value.map((e)=> {
-//     if(e.id !== item.id){
-//         bool = true
-//     }
-// })]
-
-let arr = [...state.value];
-
-arr = arr.filter((e)=> {
-    return e.id !== item.id;
-})
-
-console.log(arr);
-
+// state = {
+//     ...state,
+//     value : [...state.value , item]
+// }
 
 // console.log(state);
+
+let a = true
+let arr = [1,2,3,4,5];
+
+let state = {
+    value : arr.map((e)=> e === 4 ? true : e)
+}
+console.log(state);
